591dc55100818887222287d2e1de630865e0e7d5
Anatolii <mikov.ayu@gmail.com>
2024-07-01 22:40:52 +0500

    add relevant searching

--- app/src/test/java/hexlet/code/SearchEngineTest.java
+++ app/src/test/java/hexlet/code/SearchEngineTest.java
@@ -9,16 +9,18 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class SearchEngineTest {
+
+    public static final String DOC_1 = "I can't shoot straight unless I've had a pint!";
+    public static final String DOC_2 = "Don't shoot shoot shoot that thing at me.";
+    public static final String DOC_3 = "I'm your shooter.";
+
     @Test
     void testSearchSuccess() {
-        var doc1 = "I can't shoot straight unless I've had a pint!";
-        var doc2 = "Don't shoot shoot shoot that thing at me.";
-        var doc3 = "I'm your shooter.";
 
         List<Map<String, String>> docs = List.of(
-                Map.of("id", "doc1", "text", doc1),
-                Map.of("id", "doc2", "text", doc2),
-                Map.of("id", "doc3", "text", doc3)
+                Map.of("id", "doc1", "text", DOC_1),
+                Map.of("id", "doc2", "text", DOC_2),
+                Map.of("id", "doc3", "text", DOC_3)
         );
 
         List<String> result = SearchEngine.search(docs, "shoot");
@@ -29,19 +31,30 @@ class SearchEngineTest {
     @Test
     void testSearchEmptyDocs() {
         List<String> result = SearchEngine.search(new ArrayList<>(), "shoot");
-        assertThat(List.of()).hasSameElementsAs(result);
+        assertThat(result).hasSameElementsAs(List.of());
     }
 
     @Test
     void testSearchPunctuation() {
-        var doc1 = "I can't shoot straight unless I've had a pint!";
 
-        List<Map<String, String>> docs = List.of(Map.of("id", "doc1", "text", doc1));
+        List<Map<String, String>> docs = List.of(Map.of("id", "doc1", "text", DOC_1));
 
         List<String> result1 = SearchEngine.search(docs, "pint");
         assertThat(List.of("doc1")).hasSameElementsAs(result1);
 
         List<String> result2 = SearchEngine.search(docs, "pint!");
-        assertThat(List.of("doc1")).hasSameElementsAs(result2);
+        assertThat(result2).hasSameElementsAs(List.of("doc1"));
+    }
+
+    @Test
+    void TestSearchEngineSorted() {
+        List<Map<String, String>> docs = List.of(
+                Map.of("id", "doc1", "text", DOC_1),
+                Map.of("id", "doc2", "text", DOC_2),
+                Map.of("id", "doc3", "text", DOC_3)
+        );
+
+        List<String> result = SearchEngine.search(docs, "shoot");
+        assertThat(result).isEqualTo(List.of("doc2", "doc1"));
     }
 }
